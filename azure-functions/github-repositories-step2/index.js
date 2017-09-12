7a4608c13863e3732b7eaca1d635a7b6eb4e5e2d
cloudbeatsch <cloudbeatsch@hotmail.com>
2017-09-12 19:21:23 +0200

    included the commit history (currently only working with the latest 100 commits)

--- azure-functions/github-repositories-step2/index.js
+++ azure-functions/github-repositories-step2/index.js
@@ -4,27 +4,24 @@ let util = require('util');
 let appInsights = require("applicationinsights");
 let request = require('request-promise');
 let appInsightsClient = appInsights.getClient();
-let globalContext;
-
-
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
-function RaiseException(errorMsg, exit){
-    globalContext.log('github-repositories error occurred: ' + errorMsg);
+function RaiseException(errorMsg, exit, context){
+    context.log('github-repositories error occurred: ' + errorMsg);
 
     if(appInsightsClient.config){
         appInsightsClient.trackException(new Error(errorMsg));
-        if(exit && globalContext){
-            globalContext.log("Exiting from error " + errorMsg);
-            globalContext.done(errorMsg);
+        if(exit && context){
+            context.log("Exiting from error " + errorMsg);
+            context.done(errorMsg);
         }
     }else{
-        globalContext.log('App Insight is not properly setup. Please make sure APPINSIGHTS_INSTRUMENTATIONKEY is defined');
+        context.log('App Insight is not properly setup. Please make sure APPINSIGHTS_INSTRUMENTATIONKEY is defined');
     }
 }
 
-function executeQuery(graphRequest, queryTemplate, next) {
+function executeQuery(graphRequest, processResult, context) {
     var uri = 'https://api.github.com/graphql';
     var result = request({
         method: 'POST',
@@ -36,93 +33,75 @@ function executeQuery(graphRequest, queryTemplate, next) {
         body : graphRequest, 
         json: true
     }).then(function (result) {
-        globalContext.log(`got response: ${util.inspect(result)}`);
-        next(queryTemplate, result);
+        context.log(`got response: ${util.inspect(result)}`);
+        processResult(result, context);
     })
     .catch(function(err) {
         var errorMessage = `Could not retrieve graph: ${graphRequest}`;
-        globalContext.log(errorMessage);
+        context.log(errorMessage);
         throw new Error(errorMessage);
     });
 }
 
-function processPage(queryTemplate, graph) {
-    for (let i = 0; i < graph.data.organization.repositories.edges.length; i++) {
-        let repo = graph.data.organization.repositories.edges[i].node;
-        let document = {
-            PartitionKey : graph.data.organization.id,
-            RowKey : repo.id,
-            OrganizationName : graph.data.organization.name,
-            RepositoryName : repo.name,
-            ResourcePath: repo.resourcePath,
-            pushedAt: repo.pushedAt,
-            IsFork : repo.isFork,
-            Description : repo.description             
-        };
-        // adding the topics as properties 
-        for (let t = 0; t < repo.repositoryTopics.nodes.length; t++) {
-            let topicName = `topic_` + repo.repositoryTopics.nodes[t].topic.name.replace(/-/g,`_`);
-            document[topicName] = true;
-            globalContext.log(topicName);
-        }
-        globalContext.bindings.githubRepositoriesDocument = JSON.stringify(document);
-        globalContext.done();
+function processResult(graph, context) {
+    let document = context.bindings.githubRepositoriesStep2;
+    document[`history`] = []
+    for (let i = 0; i < graph.data.organization.repository.defaultBranchRef.target.history.edges.length; i++) {
+        document.history.push(graph.data.organization.repository.defaultBranchRef.target.history.edges[i].node);
     }
-   
-    if (graph.data.organization.repositories.pageInfo.hasNextPage) {
-        let afterString = `after:"` + graph.data.organization.repositories.pageInfo.endCursor + `",`
-        let queryString = queryTemplate.replace(`PAGINATION_PLACEHOLDER`, afterString);
-        executeQuery({query: queryString}, queryTemplate, processPage);
-    }
+    context.bindings.githubRepositoriesDocument =  JSON.stringify(document);
+    context.done();
 }
 
-function processRepositories(){
+function processRepository(organizationName, repositoryName, context){
     let queryTemplate = `{
-        organization(login: ORG_PLACEHOLDER) {
-            id
-            name
-            repositories(first: 10, PAGINATION_PLACEHOLDER orderBy: {field: PUSHED_AT, direction: DESC}) {
-            totalCount
-            edges {
-              node {
-                id
-                name
-                resourcePath  
-                pushedAt
-                repositoryTopics(first: 50) {
-                  nodes {
-                     topic {
-                      name
+            organization(login: "ORG_PLACEHOLDER") {
+              repository( name : "REPOSITORY_PLACEHOLDER") {
+                    name
+                    resourcePath
+                    pushedAt
+                    repositoryTopics(first: 10) {
+                      nodes {
+                        topic {
+                          name
+                        }
+                      }
                     }
+                    isFork
+                    description
+                    defaultBranchRef {
+                      target {
+                              ... on Commit {
+                            id
+                            history(first: 100) {
+                        edges {
+                            node {
+                            url
+                              messageHeadline
+                              oid
+                              message
+                              author {
+                                name
+                              date
+                              }
+                            }
+                          }
+                        }
+                      }
                   }
                 }
-                isFork
-                description
-              }
-            }
-            pageInfo {
-              endCursor
-              hasNextPage
             }
           }
-        }
       }`
-        let orgs = process.env.ORGANIZATIONS.split(`,`);
-        for (let o = 0; o < orgs.length; o++ ){
-            let queryTemplateOrg = queryTemplate.replace(`ORG_PLACEHOLDER`, `"` + orgs[o] + `"`);
-            let queryString = queryTemplateOrg.replace(`PAGINATION_PLACEHOLDER`, ``);
-            executeQuery({query: queryString}, queryTemplateOrg, processPage);
-        }
+      let queryTemplateOrg = queryTemplate.replace(`ORG_PLACEHOLDER`, organizationName );
+      let queryString = queryTemplateOrg.replace(`REPOSITORY_PLACEHOLDER`, repositoryName);
+      executeQuery({query: queryString}, processResult, context);
 }
 
 module.exports = function (context) {
-    globalContext = context;
-
     try{
-        globalContext.bindings.githubRepositoriesDocument =  JSON.stringify(globalContext.bindings.githubRepositoriesStep2);
-        // processRepositories();
-        globalContext.done();
+        processRepository(context.bindings.githubRepositoriesStep2.organizationLogin, context.bindings.githubRepositoriesStep2.repositoryName, context);
     } catch(error) {
-        RaiseException(error, true);
+        RaiseException(error, true, context);
     }
 }
