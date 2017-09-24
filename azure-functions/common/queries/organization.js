90921df86e806a3f8c48817683bf0746422d7ce3
cloudbeatsch <cloudbeatsch@hotmail.com>
2017-09-24 10:46:17 +0200

    refactored and minor changes

--- azure-functions/common/queries/organization.js
+++ azure-functions/common/queries/organization.js
@@ -12,7 +12,7 @@ const ORGANIZATION_QUERY = `query ($organization_name:String!, $end_cursor:Strin
     }
   }` + repositoryFragment;
 
-const ORG_USERS_QUERY = `query($organization_name:String!, $end_cursor:String) {
+const ORGANIZATION_USERS_QUERY = `query($organization_name:String!, $end_cursor:String) {
     organization(login: $organization_name) {
       members(first: 100, after: $end_cursor) {
         pageInfo {
@@ -26,9 +26,26 @@ const ORG_USERS_QUERY = `query($organization_name:String!, $end_cursor:String) {
     }
   }`
 
+  const ORGANIZATION_USERS_REPOS_QUERY = `query($organization_name:String!, $end_members_cursor:String, $end_repos_cursor:String) {
+    organization(login: $organization_name) {
+      members(first: 10, after: $end_members_cursor) {
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        nodes {
+          login
+          repositories(first: 10, after: $end_repos_cursor orderBy: {field: PUSHED_AT, direction: DESC}) {
+            ... REPOSITORY_FRAGMENT
+          }
+        }
+      }
+    }
+  }`
+
 module.exports = {
   organizationQuery: ORGANIZATION_QUERY,
-  organizationUsersQuery: ORG_USERS_QUERY
+  organizationUsersQuery: ORGANIZATION_USERS_QUERY
 }
 
 
--- azure-functions/github-repositories-step1/index.js
+++ azure-functions/github-repositories-step1/index.js
@@ -4,7 +4,7 @@ let gitHubHelper = require(`../common/githubGraphQL.js`);
 let exceptionHelper = require(`../common/exceptions.js`);
 let organizationQuery = require(`../common/queries/organization.js`).organizationQuery;
 
-function executeOrganizationQuery(organizationName, endCursor, next, context) {
+function executeRepositoryQuery(organizationName, endCursor, next, context) {
     let variables = JSON.stringify({ 
         end_cursor : endCursor,
         organization_name : organizationName
@@ -12,7 +12,7 @@ function executeOrganizationQuery(organizationName, endCursor, next, context) {
     gitHubHelper.executeQuery(organizationQuery, variables, next, context);
 }
 
-function processOrganizationRepositoryPage(graph, context) {
+function processOrganizationRepositoriesPage(graph, context) {
     for (let i = 0; i < graph.data.organization.repositories.edges.length; i++) {
         let repo = graph.data.organization.repositories.edges[i].node;
         let topics = []
@@ -21,36 +21,36 @@ function processOrganizationRepositoryPage(graph, context) {
             topics.push(repo.repositoryTopics.nodes[t].topic.name);
         }
         let document = {
-            id : graph.data.organization.id + `-` + repo.id,
-            organizationId : graph.data.organization.id,
-            repositoryId : repo.id,
-            organizationName : graph.data.organization.name,
-            organizationLogin : graph.data.organization.login,
-            repositoryOwner : graph.data.organization.login,
-            repositoryName : repo.name,
+            id: graph.data.organization.id + `-` + repo.id,
+            organizationId: graph.data.organization.id,
+            repositoryId: repo.id,
+            organizationName: graph.data.organization.name,
+            organizationLogin: graph.data.organization.login,
+            repositoryOwner: graph.data.organization.login,
+            repositoryName: repo.name,
             resourcePath: repo.resourcePath,
             pushedAt: repo.pushedAt,
-            isFork : repo.isFork,
-            description : repo.description,
-            topics : topics,             
+            isFork: repo.isFork,
+            description: repo.description,
+            topics: topics,
         };
         context.step2Messages.push(document);
     }
-   
+
     if (graph.data.organization.repositories.pageInfo.hasNextPage) {
-        executeOrganizationQuery(graph.data.organization.login, graph.data.organization.repositories.pageInfo.endCursor, processOrganizationRepositoryPage, context);
+        executeRepositoryQuery(graph.data.organization.login, graph.data.organization.repositories.pageInfo.endCursor, processOrganizationRepositoryPage, context);
     }
     else {
         context.bindings.githubRepositoriesStep2 = context.step2Messages;
         context.done();
     }
 }
-
+   
 function processRepositories(context){
     context[`step2Messages`] = [];
     let orgs = process.env.ORGANIZATIONS.split(`,`);
     for (let o = 0; o < orgs.length; o++ ){
-        executeOrganizationQuery(orgs[o], null, processOrganizationRepositoryPage, context);
+        executeRepositoryQuery(orgs[o], null, processOrganizationRepositoriesPage, context);
     }
 }
 
