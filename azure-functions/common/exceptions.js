dc50d2ff8e149a8bed34a53aa40f4949dc1ea0fe
cloudbeatsch <cloudbeatsch@hotmail.com>
2017-10-17 06:48:44 +0200

    changed error logging to use error stram

--- azure-functions/common/exceptions.js
+++ azure-functions/common/exceptions.js
@@ -4,16 +4,17 @@ let appInsights = require(`applicationinsights`);
 let appInsightsClient = appInsights.getClient();
 
 function raiseException(errorMsg, exit, context){
-    context.log('github-repositories error occurred: ' + errorMsg);
+    context.log.error('Error occurred: ' + errorMsg);
 
     if(appInsightsClient.config){
         appInsightsClient.trackException(new Error(errorMsg));
-        if(exit && context){
-            context.log("Exiting from error " + errorMsg);
-            context.done(errorMsg);
-        }
-    }else{
-        context.log('App Insight is not properly setup. Please make sure APPINSIGHTS_INSTRUMENTATIONKEY is defined');
+    } 
+    else {
+        context.log.error('App Insight is not properly setup. Please make sure APPINSIGHTS_INSTRUMENTATIONKEY is defined');
+    }
+    if(exit && context){
+        context.log("Exiting from error " + errorMsg);
+        context.done(errorMsg);
     }
 }   
 
